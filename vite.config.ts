cdfbaaf7a7068005779f83d09d00041aadb74fc9
Anat Dagan <anatdaganster@gmail.com>
2025-12-21 11:02:47 +0200

    move app to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/anatdagan.github.io/indie-dev/'
+  base: '/anatdagan.github.io/'
 })

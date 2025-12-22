eee0dc038abb6227660b2c31889836ddb6029d6c
Anat Dagan <anatdaganster@gmail.com>
2025-12-22 10:52:32 +0200

    restore issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import indieDevLogo from './assets/לוגו-removebg-preview.png'
 import anatSmallPic from './assets/PHOTO-anat-dagan-small.webp'
 import anatCropPic from './assets/PHOTO-anat-dagan-crop.webp'
-import CheckmarkSquare from './assets/checkmark-square.svg?react'
+import { MonitorCheck } from 'lucide-react';
 
 import './App.css'
 
@@ -9,37 +9,40 @@ function App() {
 
 
   return (
-    <>
-      <header className="flex justify-center items-center h-1/2 text-violet-500">
-        <div className="grow text-center">
-          <h2 >אז מה זה בעצם</h2>
-          <h1 className="text-violet-500">תכנות עצמאי?</h1>
-        </div>
+    <div className="h-full">
+      <header className="flex justify-center bg-violet-50 text-violet-500 p-2 md:p-8">
         <div>
-          <div className="logo"><img src={indieDevLogo} alt="Indie Dev Logo" width={200} height={200}/></div>
+          <div className="logo"><img src={indieDevLogo} alt="Indie Dev Logo" className="w-48 md:w-64"/></div>
+        </div>
+        <div className="grow text-center py-2">
+          <h2 className="text-violet-500 text-2xl md:text-6xl">אז מה זה בעצם</h2>
+          <h1 className="text-violet-800 font-bold text-4xl md:text-8xl p-2">תכנות עצמאי?</h1>
         </div>
       </header>
-      <main className="card">
+    
+      <main className="bg-violet-800 text-violet-50 h-full flex justify-center">
+        <div className="md:flex">
 <picture>
-  <source srcSet={anatCropPic} media="(min-width: 600px)" />
-  <img src={anatSmallPic} alt="Anat Dagan" width={300} height={300}/>
+  <source media="(min-width: 465px)" srcSet={anatCropPic}/>
+  <img src={anatSmallPic} alt="ענת דגן"/>
 </picture>
         <ul>
           <li>
-            <h3 className="text-2xl font-bold flex items-center"><span><CheckmarkSquare width={24} height={24} className="text-emerald-500 fill-current"/></span>מקצועיות</h3>
+            <h3 className="text-2xl font-bold flex items-center"><span><MonitorCheck size={44} color="#34d399" strokeWidth={3} /></span>מקצועיות</h3>
             <p>29 </p>
           </li>
           <li>
-            <h3 className="text-2xl font-bold flex items-center"><span><CheckmarkSquare width={24} height={24} className="text-emerald-500 fill-current"/></span>יחס אישי</h3>
+            <h3 className="text-2xl font-bold flex items-center"><span><MonitorCheck size={44} color="#fbbf24" strokeWidth={3} /></span>יחס אישי</h3>
             <p></p>
           </li>
           <li>
-            <h3 className="text-2xl font-bold flex items-center"><span><CheckmarkSquare width={24} height={24} className="text-emerald-500 fill-current"/></span>יצירתיות</h3>
+            <h3 className="text-2xl font-bold flex items-center"><span><MonitorCheck size={44} color="#f97316" strokeWidth={3} /></span>יצירתיות</h3>
             <p></p>
           </li>
         </ul>
+      </div>
       </main>
-    </>
+    </div>
   )
 }
 
